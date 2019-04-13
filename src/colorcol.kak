29a27399e522ab83ac8b8fd57fc01d2f97b930e9
SolitudeSF <solitudesf@protonmail.com>
2019-04-13 19:18:28 +0300

    Add max marks per line option. Version 0.1.2

--- src/colorcol.kak
+++ src/colorcol.kak
@@ -1,5 +1,6 @@
 declare-option -hidden line-specs colorcol_marks
 declare-option str colorcol_char █
+declare-option int colorcol_max_marks 3
 
 define-command colorcol-enable %{
     colorcol-refresh
@@ -9,7 +10,8 @@ define-command colorcol-disable %{
     remove-highlighter buffer/colorcol
 }
 define-command colorcol-refresh %{evaluate-commands %sh{
-    colorcol "$kak_timestamp" "$kak_buffile" "$kak_opt_colorcol_char"
+    colorcol "$kak_timestamp" "$kak_buffile" "$kak_opt_colorcol_char" \
+        "$kak_opt_colorcol_max_marks"
 } }
 define-command colorcol-auto-refresh %{
     hook global BufWritePost .* colorcol-refresh
--- src/colorcol.nim
+++ src/colorcol.nim
@@ -1,16 +1,13 @@
-import regex, os
+import regex, os, strutils
 
 const
-  kak_source = slurp "colorcol.kak"
-  regexHex = re"\b(?:([[:xdigit:]]{6})(?:[[:xdigit:]]{2})?|([[:xdigit:]]{3})(?:[[:xdigit:]])?)\b"
-
-template addLine(n: int) =
-  stdout.write " ", n, "|"
+  kakSource = slurp "colorcol.kak"
+  regexHex = re"\b#?(?:([[:xdigit:]]{6})(?:[[:xdigit:]]{2})?|([[:xdigit:]]{3})(?:[[:xdigit:]])?)\b"
 
 template addColor(color, marker: string) =
   stdout.write "{rgb:", color, "}", marker
 
-func doubleColor(c: string): string {.noinit.} =
+func doubleColor(c: string): string {.noinit, inline.} =
   result = newString(6)
   result[0] = c[0]
   result[1] = c[0]
@@ -20,16 +17,18 @@ func doubleColor(c: string): string {.noinit.} =
   result[5] = c[2]
 
 if paramCount() == 0:
-  stdout.write kak_source
+  stdout.write kakSource
   quit 0
 
 let
   timestamp = paramStr 1
   buffile = paramStr 2
   marker = paramStr 3
+  maxMarks = parseInt paramStr 4
 
 var
   n = 0
+  matches = 0
   lineMarked = false
 
 stdout.write "set-option buffer colorcol_marks ", timestamp
@@ -37,12 +36,15 @@ stdout.write "set-option buffer colorcol_marks ", timestamp
 if existsFile buffile:
   for line in buffile.lines:
     inc n
+    matches = 0
     lineMarked = false
     for match in line.findAll(regexHex):
       if not lineMarked:
-        addLine n
+        stdout.write " ", n, "|"
         lineMarked = true
       for slice in match.group(0):
         addColor line[slice], marker
       for slice in match.group(1):
         addColor line[slice].doubleColor, marker
+      inc matches
+      if matches == maxMarks: break
