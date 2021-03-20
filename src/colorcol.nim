ac7afdf9d1deed7c24f46a8e046e6c74153f0066
SolitudeSF <solitudesf@protonmail.com>
2021-03-20 23:25:50 +0200

    Optimize prefix parsing

--- src/colorcol.nim
+++ src/colorcol.nim
@@ -2,21 +2,24 @@ import os, strutils
 
 const kakSource = slurp "colorcol.kak"
 
-type Color = array[6, char]
+type
+  Color = array[6, char]
+  Prefix = enum
+    pNone, pR
 
 func isHexadecimal(c: char): bool = c in {'0'..'9', 'a'..'f', 'A'..'F'}
 func isValid(i: int): bool = i == 3 or i == 4 or i == 6 or i == 8
 
-func normalizedColor(str: string, start, len: int): Color {.inline.} =
-  if len >= 6:
-    copyMem addr result, unsafeAddr str[start], sizeof Color
+func normalizedColor(str: openArray[char]): Color {.inline.} =
+  if str.len >= 6:
+    copyMem addr result, unsafeAddr str[0], sizeof Color
   else:
-    result[0] = str[start]
-    result[1] = str[start]
-    result[2] = str[start + 1]
-    result[3] = str[start + 1]
-    result[4] = str[start + 2]
-    result[5] = str[start + 2]
+    result[0] = str[0]
+    result[1] = str[0]
+    result[2] = str[0 + 1]
+    result[3] = str[0 + 1]
+    result[4] = str[0 + 2]
+    result[5] = str[0 + 2]
 
 func add(s: var string, a: openArray[char]) =
   let l = s.len
@@ -56,42 +59,64 @@ func addColor(cmd: var string, color: Color, marker: string) =
   cmd.add marker
 
 iterator colorSlices(s: string): (int, Slice[int], Color) =
+
+  template submit(s: string, line, linestart, start, len, prefixLen: int): untyped =
+    yield (line, start - linestart..<start - linestart + len,
+      s.toOpenArray(start + prefixLen, start + len).normalizedColor)
+
   var
     i = 0
     len = 0
     line = 1
     linestart = 0
     start = 0
-    colorPrefix = 0
-  while i < s.len - 3:
+    prefixLen = 0
+    prefix = pNone
+
+  while i < s.len:
     if len == 0:
-      if s[i] == '#':
-        start = i
-        colorPrefix = 1
-        len = 1
-      elif s[i] == '\n':
-        inc line
-        linestart = i + 1
-      elif s[i..i + 3] == "rgb:":
-        start = i
-        colorPrefix = 4
-        len = 4
-        i += 3
+      case prefix
+      of pNone: discard
+      of pR:
+        if prefixLen == 1 and s[i] == 'g' or
+           prefixLen == 2 and s[i] == 'b':
+          inc prefixLen
+        elif prefixLen == 3 and s[i] == ':':
+          inc prefixLen
+          len = prefixLen
+        else:
+          prefixLen = 0
+          prefix = pNone
+
+      if prefix == pNone:
+        case s[i]
+        of '#':
+          start = i
+          prefixLen = 1
+          len = 1
+        of 'r':
+          start = i
+          prefixLen = 1
+          prefix = pR
+        of '\n':
+          inc line
+          linestart = i + 1
+        else:
+          discard
+
+      stderr.writeLine prefix
     else:
       if s[i].isHexadecimal:
         inc len
       else:
-        if not s[i].isAlphaNumeric and (len - colorPrefix).isValid:
-          yield (line, start - linestart..<start - linestart + len,
-            normalizedColor(s, start + colorPrefix, len))
+        if not s[i].isAlphaNumeric and (len - prefixLen).isValid:
+          submit(s, line, linestart, start, len, prefixLen)
         if s[i] == '\n':
           inc line
           linestart = i + 1
         len = 0
     inc i
-  if len.isValid:
-    yield (line, start - linestart..<start - linestart + len,
-      normalizedColor(s, start + 1, len))
+  if (len - prefixLen).isValid: submit(s, line, linestart, start, len, prefixLen)
 
 proc main =
   if paramCount() == 0:
